5f2b76a303fc1150b31942f9a44637542de0d0a7
Gabriel Costa <gabriel_costa@outlook.com>
2018-01-02 18:59:03 -0200

    Seção 2, Aula 7 - Final

--- variaveis avançado.go
+++ variaveis avançado.go
@@ -9,9 +9,18 @@ var (
 	quantidade         int
 	comprou            bool
 	valor              float64
-	palavras           rune
+	palavras           rune //sem maiúscula é privada, com maiúscula é pública! públicas precisam de comentários
+	//e declarações individuais
+
+	//Saidinha é bem saidinha e gosta de passear lá fora xD
+	Saidinha rune
 )
 
-func mainVA() {
+func main() {
+	teste := "valor de teste"
+	fmt.Printf("endereço: %s\r\n", endereco)
+	fmt.Printf("telefone: %s\r\n", telefone)
+	fmt.Printf("quantidade: %d\r\n", quantidade)
+	fmt.Printf("teste! arrrrg! - %v\r\n", teste)
 	fmt.Printf("palavras rúnicas arrrrg! - %v\r\n", palavras)
 }
