db51dec6e18f4eab1ffaae01a7297ae12f2e627e
Gabriel Costa <gabriel_costa@outlook.com>
2018-01-02 18:34:24 -0200

    Seção2, Aula 7 - Variáveis (Avaçado)

---
+++ variaveis avançado.go
@@ -0,0 +1,17 @@
+package main
+
+import (
+	"fmt"
+)
+
+var (
+	endereco, telefone string
+	quantidade         int
+	comprou            bool
+	valor              float64
+	palavras           rune
+)
+
+func mainVA() {
+	fmt.Printf("palavras rúnicas arrrrg! - %v\r\n", palavras)
+}
