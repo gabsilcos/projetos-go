f6e4d7d32cd6e5f07a14284174b9d597402c51de
Gabriel Costa <gabriel_costa@outlook.com>
2018-01-02 21:59:11 -0200

    Seção 2, Aula 8 - Packages

---
+++ pacotes/main.go
@@ -0,0 +1,11 @@
+package main
+
+import "fmt"
+
+//NomeDoUsuario é Nome do usuário do Sistema
+var NomeDoUsuario = "BB"
+
+func main() {
+	fmt.Printf("Nome do Usuário: %v\r\n", NomeDoUsuario)
+	fmt.Printf("Prefixo da capital: %d\r\n", prefixo.Capital)
+}
---
+++ pacotes/prefixo/numeros.go
@@ -0,0 +1,4 @@
+package prefixo
+
+//Capital representa o número do prefixo de telefonia da capital de um estado/província
+var Capital = 21
