c342ccce8e36b9f31d220bfd7d65c93b018d4adc
Gabriel Costa <gabriel_costa@outlook.com>
2018-01-04 17:16:44 -0200

    Seção 2, Aula 10 - Funções [Avançado]

--- src/github.com/user/funcoes_basico/main.go
+++ src/github.com/user/funcoes_basico/main.go
@@ -10,4 +10,8 @@ func main(){
 	fmt.Printf("o resultado da multiplicação é: %d\r\n", resultado)
 	resultado = matematica.Soma(2,3)
 	fmt.Printf("o resultado da soma é: %d\r\n", resultado)
+	resultado = matematica.Calculo(matematica.Divisor,8,4)
+	fmt.Printf("o resultado da divisão é: %d\r\n", resultado)
+	resultado, resto := matematica.DivisorComResto(9,4)
+	fmt.Printf("o resultado da divisão é: %d e o resto é %d\r\n", resultado,resto)
 }
--- src/github.com/user/funcoes_basico/matematica/generico.go
+++ src/github.com/user/funcoes_basico/matematica/generico.go
@@ -12,3 +12,16 @@ func Calculo(funcao func(int, int) int, a int, b int) int {
 func Multiplicador(x int, y int) int {
 	return x*y
 }
+
+//Divisor d
+func Divisor(x int, y int) (resultado int){ //retorno assinado
+	resultado = x/y
+	return
+}	
+
+//DivisorComResto dd
+func DivisorComResto(x int, y int) (resultado int, resto int) {
+	resultado = x/y
+	resto = x%y
+	return
+}
